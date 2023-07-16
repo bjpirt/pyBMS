b076bc108b931051e042144e197cdd4d2c7b5b03
Ben Pirt <ben@pirt.co.uk>
2023-07-16 10:24:26 +0100

    Improve the debug output

--- bms/bms.py
+++ bms/bms.py
@@ -57,9 +57,13 @@ class Bms:
     def print_debug(self):
         if not self.battery_pack.ready:
             print("Battery pack not ready")
+        else:
+            print(
+                f"Modules: {len(self.battery_pack.modules)} Alerts: {self.battery_pack.alerts}"
+                f" Faults: {self.battery_pack.faults}")
         for i, module in enumerate(self.battery_pack.modules):
             print(
-                f"Module: {i} Voltage: {module.voltage} Temperature: \
-                    {module.temperatures[0]} {module.temperatures[1]} Fault: {module.fault}")
+                f"Module: {i} Voltage: {module.voltage} Temperature: {(module.temperatures[0])}"
+                f" {module.temperatures[1]} Alerts: {module.alerts} Faults: {module.faults}")
             for j, cell in enumerate(module.cells):
-                print(f"  |- Cell: {j} voltage: {cell.voltage}")
+                print(f"  |- Cell: {j} voltage: {cell.voltage} Alerts: {cell.alerts} Faults: {cell.faults}")
